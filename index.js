ac518cda604fcdf4d75c64747dd6ed35b2b3af21
j92pruitt <j92pruitt@gmail.com>
2021-07-29 20:32:19 -0700

    Upgraded database connection to use async await.

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 require('dotenv').config();
 
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: process.env.DB_USER,
-      // MySQL password
-      password: process.env.DB_PASS,
-      database: 'employee_db'
-    },
-);
+async function main() {
+    const db = await mysql.createConnection(
+        {
+        host: 'localhost',
+        // MySQL username,
+        user: process.env.DB_USER,
+        // MySQL password
+        password: process.env.DB_PASS,
+        database: 'employee_db'
+        },
+    );
 
-db.query('SELECT * FROM employee', (err, results) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(results);
-    }
-});
+    const [rows,] = await db.query('SELECT * FROM employee');
+
+    console.table(rows)
+
+    // db.query('SELECT * FROM employee', (err, results) => {
+    //     if (err) {
+    //         console.error(err);
+    //     } else {
+    //         console.log(results);
+    //     }
+    // });
+}
+
+main();
