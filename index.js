2117557c34abf2e8b6a7ca4e87d1a9ee3436bebf
j92pruitt <j92pruitt@gmail.com>
2021-07-30 19:45:33 -0700

    Completed control flow for program and resolved problem with program not exiting on completion.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
 
-async function main() {
+async function init() {
     const db = await mysql.createConnection(
         {
         host: 'localhost',
@@ -19,12 +19,10 @@ async function main() {
 
     // console.table(rows);
 
-    userChoice = await getUserSelection();
-    console.log(`User choice is: ${userChoice}`)
-
+    return getUserChoice();
 }
 
-async function getUserSelection(){
+async function getUserChoice(){
     const questions = [
         {
             type: "list",
@@ -37,14 +35,81 @@ async function getUserSelection(){
                 "Add a Role",
                 "View all Employees",
                 "Add an Employee",
-                "Update an Employee"
+                "Update an Employee",
+                "Quit"
             ]
         }
     ]
 
     const answer = await inquirer.prompt(questions);
 
-    return answer.dbAction
+    return handleUserChoice(answer.dbAction)
 }
 
-main();
+function handleUserChoice (userChoice) {
+
+    if (userChoice === "View all Departments") {
+        selectDepartments();
+    } else if (userChoice === "Add a Deparment") {
+        createDepartment();
+    } else if (userChoice === "View all Roles") {
+        selectRoles();
+    } else if (userChoice === "Add a Role") {
+        createRole();
+    } else if (userChoice === "View all Employees") {
+        selectEmployees();
+    } else if (userChoice === "Add an Employee") {
+        createEmployee();
+    } else if (userChoice === "Update an Employee") {
+        updateEmployee();
+    }else if ("Quit") {
+        return process.exit()
+    } else {
+        return Error("Unexpected User Chioce")
+    }
+
+}
+
+// TODO: Create function to handle select departments.
+function selectDepartments() {
+    console.log("Select Dapartments Chosen");
+    return getUserChoice()
+}
+
+// TODO: Create function to handle creating new departments.
+function createDepartment() {
+    console.log("Create Dapartment Chosen");
+    return getUserChoice()
+}
+
+// TODO: Create function to handle select roles.
+function selectRoles() {
+    console.log("Select Roles Chosen");
+    return getUserChoice()
+}
+
+// TODO: Create function to handle creating new roles.
+function createRole() {
+    console.log("Create Role Chosen");
+    return getUserChoice()
+}
+
+// TODO: Create function to handle select employees.
+function selectEmployees() {
+    console.log("Select Employees Chosen");
+    return getUserChoice()
+}
+
+// TODO: Create function to handle creating new employees.
+function createEmployee() {
+    console.log("Create Employee Chosen");
+    return getUserChoice()
+}
+
+// TODO: Create function to handle updating existing employees.
+function updateEmployee() {
+    console.log("Update Employee Chosen");
+    return getUserChoice()
+}
+
+init();
