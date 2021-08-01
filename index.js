e60e84ac698f4226200c24ca3765b40cc24b4799
j92pruitt <j92pruitt@gmail.com>
2021-07-31 19:17:24 -0700

    Added logic for creating a new role.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const mysql = require('mysql2/promise');
 
 let db;
 
+/**
+ * Initializes program with a database connection
+ * @returns /Return is a call to {@link getUserChoice}
+ */
 async function init() {
      db = await mysql.createConnection(
         {
@@ -20,6 +24,10 @@ async function init() {
     return getUserChoice();
 }
 
+/**
+ * Gets input from user on what action they would like to take with the database.
+ * @returns /Return is a call {@link handleUserChoice}
+ */
 async function getUserChoice(){
     const questions = [
         {
@@ -44,6 +52,11 @@ async function getUserChoice(){
     return handleUserChoice(answer.dbAction)
 }
 
+/**
+ * Switchboard which calls the correct function based on provided input.
+ * @param {string} userChoice 
+ * @returns /Returns an error if choice is not on the list.
+ */
 function handleUserChoice (userChoice) {
 
     if (userChoice === "View all Departments") {
@@ -71,7 +84,7 @@ function handleUserChoice (userChoice) {
 /**
  * Gets data from provided table in database and displays it in a console table.
  * @param {string} table 
- * @returns 
+ * @returns /Return is a call to {@link getUserChoice}
  */
 async function selectFrom(table) {
     
@@ -82,7 +95,10 @@ async function selectFrom(table) {
     return getUserChoice()
 }
 
-// TODO: Create function to handle creating new departments.
+/**
+ * Creates a new record in the departments table using the user supplied name.
+ * @returns 
+ */
 async function createDepartment() {
     
     const answer = await inquirer.prompt(
@@ -102,11 +118,47 @@ async function createDepartment() {
     return getUserChoice()
 }
 
-// TODO: Create function to handle creating new roles.
-function createRole() {
-    console.log("Create Role Chosen");
+/**
+ * Creates a new record in the roles table, using supplied name and salary. Queries database for current list of departments for the user to select from the enforce database relations.
+ * @returns /Return is a call to {@link getUserChoice}
+ */
+async function createRole() {
+
+    const [currentDepartments,] = await db.query('SELECT * FROM department');
+
+    const departmentName = currentDepartments.map( (record) => record.name );
+    
+    const answer = await inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'roleName',
+                message: 'What is the name of the new role?'
+            },
+            {
+                type: 'input',
+                name: 'roleSalary',
+                message: 'What is the salary?'
+            },
+            {
+                type: 'list',
+                name: 'departmentName',
+                message: 'What department does this role belong to?',
+                choices: departmentName
+            }
+        ]
+    );
+    
+    const indexOfUserChoice = departmentName.indexOf(answer.departmentName);
+
+    const departmentID = currentDepartments[indexOfUserChoice].id;
+
+    const [dbresponse,] = await db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${answer.roleName}", ${parseInt(answer.roleSalary)}, ${departmentID})`);
+
+    console.log(`New role created with id: ${dbresponse.insertId}`);
+
     return getUserChoice()
-}
+};
 
 // TODO: Create function to handle creating new employees.
 function createEmployee() {
